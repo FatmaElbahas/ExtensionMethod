a4ead90562dc416aedbbe83416cb3b83aa91fd45
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2025-01-08 18:48:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String mytext = "Fatom";
+            Console.WriteLine(mytext.AddHello());
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ExtensionMethod
+{
+    public  static class StringExtensions
+    {
+       
+        public static string AddHello(this String str)
+        {
+            return "Hello "+str;
+        }
+    }
+}
